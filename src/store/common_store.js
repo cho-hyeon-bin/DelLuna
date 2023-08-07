eef17cca0baf5e0772442e93df7445d4e01c81e7
조현빈 <insideb94@gmail.com>
2023-08-07 17:45:05 +0900

    환경설정

---
+++ src/store/common_store.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+// import axios from "@/axios";
+
+export const commonStore = defineStore("common_store", {
+	id: "common_store",
+	state: () => {
+		return {};
+	},
+	actions: {
+		
+	},
+});
